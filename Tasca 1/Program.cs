ad119fd41b130c6adaecf6ded368610348d6bad3
Ferran <ferranmusan@gmail.com>
2025-05-23 17:40:54 +0200

    Pujat

---
+++ Tasca 1/Program.cs
@@ -0,0 +1,25 @@
+﻿int valor = int.Parse(Console.ReadLine());
+// Comprovar que el valor és positiu
+List<int> multiples = Metodes.ComptadorMultiples(valor);
+
+Console.WriteLine("Multiples de 7 menors que " + valor + ":");
+foreach (int m in multiples)
+{
+    Console.WriteLine(m);
+}
+
+internal static class Metodes
+{
+    public static List<int> ComptadorMultiples(int v)
+    {
+        List<int> multiples = new List<int>();
+        for (int i = 1; i < v; i++)
+        {
+            if (i % 7 == 0)
+            {
+                multiples.Add(i);
+            }
+        }
+        return multiples;
+    }
+}
